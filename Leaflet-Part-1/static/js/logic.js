239f984f5c4762808da24b2456ce8d77c9fc625e
cburgyan <cburgyan@gmail.com>
2023-09-24 14:11:38 -0500

    complete roughly earthquake markers with varying size with magnitude and depth with color

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,4 +1,5 @@
 let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+const depthColors = ['#8f0', '#ee0', '#eb0', '#e80', '#b60', '#a00'];
 
 
 function createMarkers(earthquakeData, myMap) {
@@ -9,20 +10,57 @@ function createMarkers(earthquakeData, myMap) {
         let magnitude = feature.properties.mag;
 
         let fillColor1 = "";
-        if (depth > 15){
-            fillColor1 = 'yellow';
+        if (depth > 90){
+            fillColor1 = depthColors[5];
+        } else if (depth > 70){
+            fillColor1 = depthColors[4];
+        } else if (depth > 50){
+            fillColor1 = depthColors[3];
+        } else if (depth > 30){
+            fillColor1 = depthColors[2];
+        } else if (depth > 10){
+            fillColor1 = depthColors[1];
         } else {
-            fillColor1 = 'blue';
+            fillColor1 = depthColors[0];
         }
         
         L.circle(latLng, {
             fillOpacity: 0.75,
-            color: 'white',
+            color: 'black',
+            weight: .3,
             fillColor: fillColor1,
             radius: magnitude * 10000
-        }).addTo(myMap);
+        }).bindPopup("hi").addTo(myMap);
     }
   
+  }
+
+
+  function createLegend(myMap){
+    let legend = L.control({position: 'bottomright'});
+    legend.onAdd = function() {
+        let div = L.DomUtil.create('div', 'legend');
+        div.setAttribute('style', 'background-color: white; margin-left: 0px; padding: 0px 10px 0px 0px;');
+        let labels = [];
+
+        let legendHTMLString = "<ul style=\"list-style-type: none; padding-left: 10px;\">";
+        for (let i = 0; i < depthColors.length; i++){
+            if (i == 0){
+                legendHTMLString += `<li style=\"list-style-type: none;\"><span style=\"background-color: ${depthColors[i]};\">&emsp;</span> < 10</li>`;
+            } else if (i == depthColors.length - 1){
+                legendHTMLString += `<li style=\"list-style-type: none;\"><span style=\"background-color: ${depthColors[i]};\">&emsp;</span> > 90</li>`;
+            } else {
+                legendHTMLString += `<li style=\"list-style-type: none;\"><span style=\"background-color: ${depthColors[i]};\">&emsp;</span> ${i * 20 - 10}-${i * 20 + 10}</li>`;
+            }
+        }
+
+        legendHTMLString += "</ul>";
+        div.innerHTML = legendHTMLString;
+        // console.log(div.innerHTML);
+        return div;
+    }
+
+    legend.addTo(myMap);
   }
   
   
@@ -40,6 +78,8 @@ function createMarkers(earthquakeData, myMap) {
     });
 
     createMarkers(earthquakeData, myMap);
+
+    createLegend(myMap);
   }
 
 d3.json(url).then(createMap);
